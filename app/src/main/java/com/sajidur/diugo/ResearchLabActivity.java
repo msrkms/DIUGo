9649db39358a2c9a588b468cfb77b25854e11d82
msrkms <sajidur35-1831@diu.edu.bd>
2019-10-17 04:29:29 +0600

    Research Lab Acivity Added

---
+++ app/src/main/java/com/sajidur/diugo/ResearchLabActivity.java
@@ -0,0 +1,14 @@
+package com.sajidur.diugo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResearchLabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_research_lab);
+    }
+}
