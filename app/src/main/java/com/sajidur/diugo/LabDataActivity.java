e65fa10baca8742ed613277ca1c64ff621fe650e
msrkms <sajidur35-1831@diu.edu.bd>
2019-10-21 22:05:25 +0600

    ComputerBooking Activity added

--- app/src/main/java/com/sajidur/diugo/LabDataActivity.java
+++ app/src/main/java/com/sajidur/diugo/LabDataActivity.java
@@ -5,7 +5,9 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.sajidur.diugo.Backend.Computers;
@@ -30,25 +32,32 @@ public class LabDataActivity extends AppCompatActivity {
                     .setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(LabDataActivity.this,Dashboard.class));
                             LabDataActivity.this.finish();
                         }
                     })
                     .show();
         }else if(computersArrayList.size()<1){
+
             new MaterialAlertDialogBuilder(LabDataActivity.this)
                     .setTitle("Confirm")
                     .setMessage("Data Not Received From Server,Please Try Again")
                     .setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(LabDataActivity.this,Dashboard.class));
                             LabDataActivity.this.finish();
                         }
                     })
                     .show();
         }
-        RecyclerView recyclerView= (RecyclerView) findViewById(R.id.recycler_view);
-        RecyclerViewAdapterComputers recyclerViewAdapter= new RecyclerViewAdapterComputers(this,computersArrayList);
+        final RecyclerView recyclerView= (RecyclerView) findViewById(R.id.recycler_view);
+        final RecyclerViewAdapterComputers recyclerViewAdapter= new RecyclerViewAdapterComputers(this,computersArrayList);
         recyclerView.setAdapter(recyclerViewAdapter);
         recyclerView.setLayoutManager(new GridLayoutManager(this,4));
+
+
     }
+
+
 }
