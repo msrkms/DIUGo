5f59c2508a56a519aca7097b530b9e53e5025828
msrkms <sajidur35-1831@diu.edu.bd>
2019-10-24 15:13:44 +0600

    Adminstrative Contact Added

--- app/src/main/java/com/sajidur/diugo/AdministrativeOfficialsActivity.java
+++ app/src/main/java/com/sajidur/diugo/AdministrativeOfficialsActivity.java
@@ -2,17 +2,26 @@ package com.sajidur.diugo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
+import com.sajidur.diugo.Backend.DataHold;
 
 import org.w3c.dom.Text;
 
 public class AdministrativeOfficialsActivity extends AppCompatActivity {
+    MaterialCardView materialCardViewTopManagement,materialCardViewDirector;
 TextView topmanagementtxt,directortxt,jointdirectortxt,deputyregistertxt,seniorassistantregistertxt,assistantregistertxt,seniorofficertxt,officertxt,assistantofficertxt,officeassistanttxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrative_officials);
+
+        materialCardViewTopManagement=(MaterialCardView) findViewById(R.id.topmanagement);
+        materialCardViewDirector=(MaterialCardView) findViewById(R.id.director);
 
         topmanagementtxt=(TextView)findViewById(R.id.managementtoptext);
         directortxt=(TextView)findViewById(R.id.directortext);
@@ -35,6 +44,31 @@ TextView topmanagementtxt,directortxt,jointdirectortxt,deputyregistertxt,seniora
         officertxt.setSelected(true);
         assistantofficertxt.setSelected(true);
         officeassistanttxt.setSelected(true);
+
+
+
+
+        materialCardViewTopManagement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DataHold.DataGetsFor=DataHold.TopLevelManagement;
+                DataHold.ActivityHeader="Top Management";
+                intentToEmployeeList();
+            }
+        });
+
+        materialCardViewDirector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DataHold.DataGetsFor=DataHold.Director;
+                DataHold.ActivityHeader="Director";
+                intentToEmployeeList();
+            }
+        });
+    }
+
+    private void  intentToEmployeeList(){
+        startActivity(new Intent(AdministrativeOfficialsActivity.this,EmployeeDetailsActivity.class));
 
     }
 }
